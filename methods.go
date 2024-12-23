a092ee9e783e858100ee5c5a7ec6df7e7b2a58c9
黑虫师兄 <258176257@qq.com>
2024-12-23 11:50:42 +0800

    Update methods.go

--- methods.go
+++ methods.go
@@ -4215,6 +4215,12 @@ type CreateInvoiceLinkParams struct {
 	// (https://t.me/BotNews/90).
 	Prices []LabeledPrice `json:"prices"`
 
+
+	//The number of seconds the subscription will be active for before the next payment. The currency must be set to “XTR” (Telegram Stars) 
+	//if the parameter is used. Currently, it must always be 2592000 (30 days) if specified. Any number of subscriptions can be active
+	//for a given bot at the same time, including multiple concurrent subscriptions from the same user. Subscription price must no exceed 2500 Telegram Stars.
+	SubscriptionPeriod int  `json:"subscription_period,omitempty"`
+
 	// MaxTipAmount - Optional. The maximum accepted amount for tips in the smallest units of the currency
 	// (integer, not float/double). For example, for a maximum tip of US$ 1.45 pass max_tip_amount = 145. See the
 	// exp parameter in currencies.json (https://core.telegram.org/bots/payments/currencies.json), it shows the
